23887b787a91cda55f5652022db841575b0dabe1
styv <styv2301@gmail.com>
2025-02-02 21:14:06 -0500

    feat: implement password encryption and decryption using Crypto module, add viewPassword endpoint, and update user service methods

--- src/shared/crypto/crypto.service.ts
+++ src/shared/crypto/crypto.service.ts
@@ -1,13 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 // import crypto from 'crypto';
 const crypto = require('crypto');
 
-// import { PrismaService } from 'src/prisma.service';
-
-// Clave secreta (debe almacenarse en variables de entorno)
 const SECRET_KEY = process.env.SECRET_KEY;
-const IV_LENGTH = 16;
-console.log(SECRET_KEY);
+const IV_LENGTH = 12;
+const SALT = process.env.SALT;
+const encryptionAlgorithm = process.env.ENCRYPTION_ALGORITHM;
 
 @Injectable()
 export class CryptoService {
@@ -15,45 +13,45 @@ export class CryptoService {
 
   encryptPassword(password: string): string {
     const iv = crypto.randomBytes(IV_LENGTH); // Generar IV aleatorio
-    const key = crypto.scryptSync(SECRET_KEY, iv, 32);
-    const cipher = crypto.createCipheriv(
-      'aes-256-cbc',
-      key,
-      // Buffer.from(SECRET_KEY),
-      iv,
-    );
+    const key = crypto.scryptSync(SECRET_KEY, SALT, 32);
+    const cipher = crypto.createCipheriv(encryptionAlgorithm, key, iv);
     let encrypted = cipher.update(password, 'utf8', 'hex');
     encrypted += cipher.final('hex');
-    // const authTag = cipher.getAuthTag().toString('hex'); // Obtener autenticación
-    // return iv.toString('hex') + ':' + encrypted + ':' + authTag;
-    return iv.toString('hex') + ':' + encrypted;
+    const authTag = cipher.getAuthTag().toString('hex'); // Obtener autenticación
+    // return iv.toString('hex') + ':' + encrypted;
+    return iv.toString('hex') + ':' + encrypted + ':' + authTag;
   }
 
-  decryptPassword(encryptedPassword: string): string {
-    const parts = encryptedPassword.split(':');
-    // if (parts.length !== 3) throw new Error('Formato de cifrado inválido');
+  decryptPassword(encryptedPassword: string) {
+    try {
+      const parts = encryptedPassword.split(':');
+      if (parts.length !== 3) throw new Error('Formato de cifrado inválido');
 
-    const iv = Buffer.from(parts[0], 'hex');
-    const encryptedText = parts[1];
-    // const authTag = Buffer.from(parts[2], 'hex');
+      const iv = Buffer.from(parts[0], 'hex');
+      const encryptedText = parts[1];
+      const authTag = Buffer.from(parts[2], 'hex');
 
-    const key = crypto.scryptSync(SECRET_KEY, iv, 32);
+      const key = crypto.scryptSync(SECRET_KEY, SALT, 32);
 
-    const decipher = crypto.createDecipheriv(
-      'aes-256-cbc',
-      key,
-      // Buffer.from(SECRET_KEY),
-      iv,
-    );
-    // decipher.setAuthTag(authTag);
+      const decipher = crypto.createDecipheriv(encryptionAlgorithm, key, iv);
+      decipher.setAuthTag(authTag);
 
-    let decrypted = decipher.update(encryptedText, 'hex', 'utf8');
-    decrypted += decipher.final('utf8');
+      let decrypted = decipher.update(encryptedText, 'hex', 'utf8');
+      decrypted += decipher.final('utf8');
 
-    return decrypted;
+      return decrypted;
+    } catch (error) {
+      throw new InternalServerErrorException(error.message);
+    }
   }
 
   compare(password: string, encryptedPassword: string): boolean {
-    return this.decryptPassword(encryptedPassword) === password;
+    // return this.decryptPassword(encryptedPassword) === password;
+    try {
+      const decrypted = this.decryptPassword(encryptedPassword);
+      return decrypted === password;
+    } catch (e) {
+      return false;
+    }
   }
 }
