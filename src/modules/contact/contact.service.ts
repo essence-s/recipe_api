2823442e830da906406e44aeabecefb66b17b4de
styv <styv2301@gmail.com>
2025-03-28 10:34:28 -0500

    feat: add Contact and Product entities with DTOs and services

---
+++ src/modules/contact/contact.service.ts
@@ -0,0 +1,35 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { CreateContactDto } from './dto/create-contact.dto';
+import { UpdateContactDto } from './dto/update-contact.dto';
+import { PrismaService } from 'src/prisma.service';
+
+@Injectable()
+export class ContactService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(createContactDto: CreateContactDto) {
+    try {
+      return await this.prisma.contact.create({
+        data: createContactDto,
+      });
+    } catch (error) {
+      throw new BadRequestException('Error creating contact');
+    }
+  }
+
+  findAll() {
+    return `This action returns all contact`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} contact`;
+  }
+
+  update(id: number, updateContactDto: UpdateContactDto) {
+    return `This action updates a #${id} contact`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} contact`;
+  }
+}
